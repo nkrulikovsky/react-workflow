286d000e711fd7d146d92cec029a461369d97790
nkrulikovsky <nkrulikovskiy@gmail.com>
2022-03-05 20:04:05 +0200

    ci test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const handleClick = () => {
+    console.log('first')
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleClick}>test</button>
       </header>
     </div>
   )
